9c9c58c47a097b98c07edaa4abda5e33a70fd30d
hanahuw <hana.huwai@gmail.com>
2020-12-29 00:50:58 +0700

    mengganti method view index3 menjadi home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('index3');
+		return view('home');
 	}
 
 	//--------------------------------------------------------------------
